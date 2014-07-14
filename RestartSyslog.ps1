09c17a98cefe1bf06a185a0f0d098649b4ac388a
Alex Maccabi <alex.maccabi@leverate.com>
2014-07-14 12:15:39 +0300

    created function
    function to restart syslog server by cluster

--- RestartSyslog.ps1
+++ RestartSyslog.ps1
@@ -1,5 +1,7 @@
+function restart-syslog($cluster) {
 foreach ($esxhost in (Get-Cluster -Name $cluster | Get-VMHost )){
     Write-Host "ESX: $esxhost"
     $esxcli = Get-EsxCli -VMhost $esxhost    
     $esxcli.system.syslog.reload()
+	}
 }
