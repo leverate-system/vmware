92d460209bd21726b8837b63ee9cfb7ff600691f
Alex Maccabi <alex.maccabi@leverate.com>
2014-07-14 10:16:40 +0300

    Restart esxi logs
    Restart by Cluster name

---
+++ RestartSyslog.ps1
@@ -0,0 +1,5 @@
+foreach ($esxhost in (Get-Cluster -Name $cluster | Get-VMHost )){
+    Write-Host "ESX: $esxhost"
+    $esxcli = Get-EsxCli -VMhost $esxhost    
+    $esxcli.system.syslog.reload()
+}
