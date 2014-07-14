edfaa073501d06dd54f705d9755c3e56019368eb
Alex Maccabi <alex.maccabi@leverate.com>
2014-07-14 14:42:31 +0300

    NTP Function

---
+++ ESXiNTPServer.ps1
@@ -0,0 +1,10 @@
+﻿Function NTPupdate($Cluster) {
+$Hosts = Get-Cluster $Cluster | Get-VMHost
+ForEach ($Host in $Hosts)
+{
+Remove-VmHostNtpServer -NtpServer "<old ntp server>" -VMHost $Host | Out-Null  #Remove OLD NTP
+Add-VmHostNtpServer -NtpServer "<new ntp server>" -VMHost $Host | Out-Null #Add new NTP
+Get-VmHostService -VMHost $Host | Where-Object {$_.key -eq "ntpd"} | Restart-VMHostService -Confirm:$false | Out-Null
+write "NTP Server was changed on $Host"
+}
+}
